ad3d323e151bee57f57a78a438837d3ae8854ea7
NailyaDzha <botan4091@gmail.com>
2021-10-30 18:27:30 +0200

    first commit

---
+++ src/main/java/domain/RepostsInfo.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class RepostsInfo {
+    private int count;
+    private boolean currentUserReposted;
+    //...
+    // + get/set на все поля
+}
---
+++ src/main/java/domain/VkPost.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class VkPost {
+    private int ID;
+    private String logoUrl;
+    private String imageUrl;
+    private int data;
+    private String titleText;
+    private String postText;
+    private String textUrl;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+}
